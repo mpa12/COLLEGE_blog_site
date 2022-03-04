72ad44b85ca65cd4981e964cd3244cc8c4156b02
mpa12 <maksim.28.05@mail.ru>
2022-03-04 16:26:32 +1000

    1.0.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-//pass
+let phrases = [
+  { text: 'ПРИВЕТ МИР!' },
+  { text: 'HELLO WORDL!' },
+  { text: 'ПРИВІТ СВІТ!' },
+  { text: 'HALLO WELT!' },
+  { text: 'POZDRAV SVIJETE!' }
+];
+
+let phrase = document.querySelector('.hello');
+let countPhrase = 0;
+
+function changePhrase() {
+	if (countPhrase >= phrases.length) {
+		countPhrase = 0;
+	}
+	smoothly(phrase, 'textContent', phrases[countPhrase].text);
+}
+
+setInterval(function() {
+	countPhrase += 1;
+	changePhrase();
+}, 2000);
