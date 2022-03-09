406e079eddf5b926ee1f7ce55f624ccb0ed74ff4
mpa12 <maksim.28.05@mail.ru>
2022-03-09 19:02:28 +1000

    1.3.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,86 @@ setInterval(function() {
 	countPhrase += 1;
 	changePhrase();
 }, 2000);
+
+window.onload = function() {
+	let objsForAnimate = [];
+
+	function changeObjs() {
+		let obj = document.getElementsByClassName('label')[0];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('label')[1];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('photo__img')[0];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('photo__img')[1];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('description')[0];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('description')[1];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('work')[0];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('work')[1];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('work')[2];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('work')[3];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('work')[4];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+
+		obj = document.getElementsByClassName('urls')[0];
+		objsForAnimate.push([obj, obj.offsetTop, obj.offsetTop + obj.offsetHeight]);
+	};
+
+	changeObjs();
+
+	function animate() {
+		let scrollBottom = window.scrollY + window.innerHeight;
+		let scrollTop = window.scrollY;
+
+		for (let i = 0; i < objsForAnimate.length; i++) {
+			if ((scrollBottom >= objsForAnimate[i][1])) {
+
+				let object = objsForAnimate[i][0];
+				let classActive = object.classList[0] + '-active';
+
+				if (!object.classList.contains(classActive)) {
+					object.classList.add(classActive);
+				}
+			} else {
+
+				let object = objsForAnimate[i][0];
+				let classActive = object.classList[0] + '-active';
+
+				object.classList.remove(classActive);
+			}
+
+			if (scrollTop >= objsForAnimate[i][2]) {
+
+				let object = objsForAnimate[i][0];
+				let classActive = object.classList[0] + '-active';
+
+				object.classList.remove(classActive);
+			}
+		}
+	}
+
+	window.onscroll = function() {
+		animate();
+	}
+
+	window.onresize = function() {
+		changeObjs();
+	}
+}
